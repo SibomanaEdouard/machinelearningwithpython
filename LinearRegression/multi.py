d9a2846d5b45cf49dd47907d3841927010a237bc
Edouard <edouard@kali>
2023-09-02 13:18:16 +0200

    This is other updated codes  with trained model

--- LinearRegression/multi.py
+++ LinearRegression/multi.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pn
 from sklearn import datasets,metrics,linear_model
+import seaborn as sbn
 
 # load the boston dataset
 data_url = "http://lib.stat.cmu.edu/datasets/boston"
@@ -15,3 +16,34 @@ x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.4,random_state=1)
 
 # this is to create linear regression
 reg=linear_model.LinearRegression()
+
+# let train model using training sets
+reg.fit(x_train,y_train)
+
+# let print the regression coeficient
+print("The coefficients are \n ",reg.coef_)
+
+# let me  the score where 1 means 100% perfect predictions
+print('The variance score is \n ',reg.score(x_test,y_test))
+
+# let me plot the residual errors
+
+# let me set the plotting styles
+# plt.style.use('fivethirtyeight')
+plt.style.use('dark_background')
+
+# let me plot resual errors with scatter
+plt.scatter(reg.predict(x_train),
+            reg.predict(x_train)-y_train,
+            color='indigo',label='Train data with python',s=10)
+plt.title('This is the scatter of resual errors')
+plt.xlabel('Error in unitless')
+plt.ylabel('Error in unitless')
+
+# this is plotting the line for zero error 
+plt.hlines(xmin=1,xmax=100,linewidth=2,y=1, colors='indigo')
+plt.vlines(ymin=-100,ymax=100,x=50, colors='red')
+
+# this is ploting legend
+plt.legend(loc='upper right')
+plt.show()
